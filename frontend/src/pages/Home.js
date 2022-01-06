69a3319e4b3e07b1b06a56ba26f837797baac350
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-06 17:49:34 +0100

    ajout de dossier frontend

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="vectorOne"></div>
+            <div className="vectorTwo"></div>
+            <div className="logo"><img src="./img/logo.png" alt="logo-Groupomania"/></div>
+        </div>       
+    );
+}
+
+export default Home;
